9b6120bd6c5c5dcf19f329a8a4e720b90ac1e052
klphillips <kphill33@msudenver.edu>
2018-05-04 10:47:45 -0600

    Committing files that allow recipes to be added

--- app/src/main/java/com/example/cteve/myweeklymenu/recipeList.kt
+++ app/src/main/java/com/example/cteve/myweeklymenu/recipeList.kt
@@ -3,8 +3,13 @@ package com.example.cteve.myweeklymenu
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
+import android.content.DialogInterface
+
+
 
 class recipeList : AppCompatActivity() {
 
@@ -14,14 +19,19 @@ class recipeList : AppCompatActivity() {
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.toolbar, menu)
+        menuInflater.inflate(R.menu.recipe_toolbar, menu)
         val recipe = menu!!.findItem(R.id.action_list)
         recipe.isVisible = false
+        display()
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
 
+        R.id.action_addRecipe ->{
+            addRecipe()
+            true
+        }
         R.id.action_settings -> {
             // User chose the "Settings" item, show the app settings UI...
             true
@@ -47,6 +57,22 @@ class recipeList : AppCompatActivity() {
         else -> {
             super.onOptionsItemSelected(item)
         }
+    }
+
+    fun addRecipe(){
+        val items = arrayOf<CharSequence>("Breakfast", "Lunch", "Dinner")
+        val builder = AlertDialog.Builder(this@recipeList)
+        builder.setTitle("Add Recipe")
+
+        builder.setSingleChoiceItems(items, -1, DialogInterface.OnClickListener { dialog, item -> Toast.makeText(applicationContext, items[item], Toast.LENGTH_SHORT).show() })
+
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+    }
+
+    fun display(){
+
+
     }
 }
 
