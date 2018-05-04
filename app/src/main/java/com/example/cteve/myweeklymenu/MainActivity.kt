3bdf3f839efbcfdae1317a9ceb6435d52e51c30b
cstevens4810 <cstevens4810@gmail.com>
2018-05-04 09:24:42 -0600

    Commiting Files

--- app/src/main/java/com/example/cteve/myweeklymenu/MainActivity.kt
+++ app/src/main/java/com/example/cteve/myweeklymenu/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.cteve.myweeklymenu
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,4 +10,27 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+
+        R.id.action_settings -> {
+            // User chose the "Settings" item, show the app settings UI...
+            true
+        }
+        R.id.action_generate ->{
+            val intent = Intent(this, BlendingActivity::class.java)
+            this.startActivity(intent)
+            true
+        }
+
+        R.id.action_main->{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            true
+        }
 }
