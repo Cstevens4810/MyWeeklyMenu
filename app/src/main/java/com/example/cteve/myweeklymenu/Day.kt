b5706534bd6941f2fe47c7d88f0496de1665936f
cstevens4810 <cstevens4810@gmail.com>
2018-05-04 10:54:59 -0600

    Commiting Files

---
+++ app/src/main/java/com/example/cteve/myweeklymenu/Day.kt
@@ -0,0 +1,11 @@
+package com.example.cteve.myweeklymenu
+
+/**
+ * Created by klphillips on 5/4/2018.
+ */
+
+class Day internal constructor(//What day of the week?
+        var dayOfWeek: String?, //Breakfast for that day?
+        var breakfast: Boolean?, //Lunch for that day?
+        var lunch: Boolean?, //Dinner for that?
+        var dinner: Boolean?)
---
+++ app/src/main/java/com/example/cteve/myweeklymenu/Recipe.kt
@@ -0,0 +1,12 @@
+package com.example.cteve.myweeklymenu
+
+/**
+ * Created by klphillips on 5/4/2018.
+ */
+
+class Recipe internal constructor(//Name of the recipe?
+        var name: String?, //Breakfast for that day?
+        var breakfast: Boolean?, //Lunch for that day?
+        var lunch: Boolean?, //Dinner for that?
+        var dinner: Boolean?, //Recipe itself
+        var recipe: String?)
