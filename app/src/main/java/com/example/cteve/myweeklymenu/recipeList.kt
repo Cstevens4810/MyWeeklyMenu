2afe20b61303c9b35f60b8e2f715be6662eb1c37
KyraPhillips <kphill33@msudenver.edu>
2018-05-04 14:14:33 -0600

    Committing new code

--- app/src/main/java/com/example/cteve/myweeklymenu/recipeList.kt
+++ app/src/main/java/com/example/cteve/myweeklymenu/recipeList.kt
@@ -6,12 +6,16 @@ import android.os.Bundle
 import android.support.v7.app.AlertDialog
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.Toast
 import android.content.DialogInterface
-
+import android.view.LayoutInflater
+import android.widget.*
+import kotlinx.android.synthetic.main.activity_recipe_list.*
+import kotlinx.android.synthetic.main.dialog_layout.*
 
 
 class recipeList : AppCompatActivity() {
+
+    var recipes = ArrayList<Recipe>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -60,19 +64,79 @@ class recipeList : AppCompatActivity() {
     }
 
     fun addRecipe(){
-        val items = arrayOf<CharSequence>("Breakfast", "Lunch", "Dinner")
+        val inflater = LayoutInflater.from(this@recipeList)
+        val subView = inflater.inflate(R.layout.dialog_layout, null)
+
         val builder = AlertDialog.Builder(this@recipeList)
         builder.setTitle("Add Recipe")
 
-        builder.setSingleChoiceItems(items, -1, DialogInterface.OnClickListener { dialog, item -> Toast.makeText(applicationContext, items[item], Toast.LENGTH_SHORT).show() })
+        val nameText = subView.findViewById<EditText>(R.id.recipeName) as EditText
+        val boxB = subView.findViewById<CheckBox>(R.id.breakfastBox)
+        val boxL = subView.findViewById<CheckBox>(R.id.lunchBox)
+        val boxD = subView.findViewById<CheckBox>(R.id.dinnerBox)
+        val btnCamera = subView.findViewById<Button>(R.id.cameraButton) as Button
+        val btnLibrary = subView.findViewById<Button>(R.id.libraryButton) as Button
+        val detailsText = subView.findViewById<EditText>(R.id.recipeDetails) as EditText
+
+        var breakfast = false
+        var lunch = false
+        var dinner = false
+
+        val btnSave = subView.findViewById<Button>(R.id.save) as Button
+
+        boxB.setOnCheckedChangeListener { buttonView, isChecked ->
+            breakfast = true
+        }
+        boxL.setOnCheckedChangeListener { buttonView, isChecked ->
+            lunch = true
+        }
+        boxD.setOnCheckedChangeListener { buttonView, isChecked ->
+            dinner = true
+        }
+        btnSave.setOnClickListener{
+            var rName = nameText.text.toString()
+            var rDetails = detailsText.text.toString()
+            var newRecipe = Recipe(rName, breakfast, lunch, dinner, rDetails)
+
+            recipes.add(newRecipe)
+
+            display()
+        }
+
+        builder.setView(subView)
 
         val dialog: AlertDialog = builder.create()
         dialog.show()
     }
 
     fun display(){
+        val bList = findViewById<ListView>(R.id.breakfastList)
+        val lList = findViewById<ListView>(R.id.lunchList)
+        val dList = findViewById<ListView>(R.id.dinnerList)
 
+        var tempBList = ArrayList<Recipe>()
+        var tempLList = ArrayList<Recipe>()
+        var tempDList = ArrayList<Recipe>()
+        for (recipe in recipes){
+            if (recipe.breakfast){
+                tempBList.add(recipe)
+            }
+            if (recipe.lunch){
+                tempLList.add(recipe)
+            }
+            if (recipe.dinner){
+                tempDList.add(recipe)
+            }
+        }
 
+        val adapterB = ArrayAdapter(this, android.R.layout.simple_list_item_1, tempBList)
+        bList.adapter = adapterB
+
+        val adapterL = ArrayAdapter(this, android.R.layout.simple_list_item_1, tempLList)
+        lList.adapter = adapterL
+
+        val adapterD = ArrayAdapter(this, android.R.layout.simple_list_item_1, tempDList)
+        dList.adapter = adapterD
     }
 }
 
